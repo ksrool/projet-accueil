1d7525d3f5bf2f878e51bc01511b632d9f119b75
ksrool <mouradzaitar2006@gmail.com>
2025-07-23 12:47:51 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const fadeIns = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.animationPlayState = 'running';
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.2 });
+
+fadeIns.forEach(el => {
+  observer.observe(el);
+});
